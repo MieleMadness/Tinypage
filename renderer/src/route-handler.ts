d26ce645df831221e64baeb350b1f93422aab98d
Navid Kabir <navid113@gmail.com>
2021-09-02 13:34:45 -0400

    Add redirect to editor when base rendererURL is typed.

--- renderer/src/route-handler.ts
+++ renderer/src/route-handler.ts
@@ -22,34 +22,10 @@ export class RouteHandler {
         this.fastify = fastify;
 
         fastify.setNotFoundHandler((request, reply) => {
-            reply.type('text/html').status(404);
+            reply.status(StatusCodes.PERMANENT_REDIRECT);
 
             // language=HTML
-            reply.send(`
-                <html lang="">
-                    <head>
-                        <title>${config.appName} Web Client</title>
-                        <meta charset="UTF-8">
-                        <link rel="icon" type="image/x-icon" href="/favicon.png"/>
-                        <link rel="icon" type="image/png" href="/favicon.png"/>
-                    </head>
-                    <body>
-                        <div class="w-full h-full flex flex-col items-center justify-center">
-                            <h1 class="text-4xl text-gray-900 mb-2 font-extrabold">404 - Not Found</h1>
-                            <h3 class="text-lg text-gray-600 mb-4">We couldn't find what you were looking for, sorry!</h3>
-                            <a class="bg-blue-600 hover:bg-blue-400 rounded-2xl shadow text-white py-3 px-6 text-sm font-medium" href="` + request.url + `">Reload page</a>
-                        </div>
-                        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/tailwindcss/2.0.4/tailwind.min.css"/>
-                        <style>
-                            @import url('https://rsms.me/inter/inter.css');
-                            html { font-family: 'Inter', sans-serif; }
-                            @supports (font-variation-settings: normal) {
-                            html { font-family: 'Inter var', sans-serif; }
-                            }
-                        </style>
-                    </body>
-                </html>
-            `);
+            reply.redirect(config.editorUrl);
         });
     }
 
